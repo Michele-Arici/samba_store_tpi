020a0104f2f042afe564dbf853836521cd03d6ca
zacthebigkub <boniottimattia100100@gmail.com>
2022-04-12 09:45:33 +0200

    Aggiustato tasto sign in
    Sheesh contorlla modifiche effettuate. Non ho voglia di fare copia ed incolla

--- scripts/sambaAuth.js
+++ scripts/sambaAuth.js
@@ -1,7 +1,7 @@
 import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'https://www.gstatic.com/firebasejs/9.6.10/firebase-auth.js';
 
 import { auth } from '/scripts/firebase-config.js';
-import { getCookie, setCookie} from '/scripts/sambaCookies.js';
+import { getCookie, setCookie } from '/scripts/sambaCookies.js';
 
 //REGISTRAZIONE
 const signupForm = document.querySelector('#signup_form');
@@ -11,17 +11,24 @@ signupForm.addEventListener('submit', (e) => {
     //get user info
     const email = signupForm['signup_email'].value;
     const password = signupForm['signup_password'].value;
+    const isTOS = signupForm['tos_agreement'].value;
 
-    createUserWithEmailAndPassword(auth, email, password)
-        .then((userCredential) => {
-            // Signed in 
-            const user = userCredential.user;
-        })
-        .catch((error) => {
-            const errorCode = error.code;
-            const errorMessage = error.message;
-            // ..
-     });
+    if (isTOS == 'on') {
+        createUserWithEmailAndPassword(auth, email, password)
+            .then((userCredential) => {
+                // Signed in 
+                const user = userCredential.user;
+            })
+            .catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                // ..
+            });
+    } else {
+        //#TODO sostituire il cambio di valore del tos con un alert modal
+        document.getElementById('tos_agreement').value = "E' Obbligatorio accettare le regole del TOS per continuare!";
+    }
+
 });
 
 //REGISTRAZIONE
@@ -34,11 +41,11 @@ let loginEmailCookie = getCookie("user_email");
 let loginPasswordCookie = getCookie("user_password");
 
 //se i valori dei cookie del login non sono nulli li inserisco nel form
-if(loginEmailCookie !== null ){
-    
+if (loginEmailCookie !== null) {
+
     document.getElementById("signin_email").value = loginEmailCookie;
 
-    if(loginPasswordCookie !== null ){
+    if (loginPasswordCookie !== null) {
         document.getElementById("signin_password").value = loginPasswordCookie;
     }
 
@@ -61,12 +68,12 @@ signinForm.addEventListener('submit', (e) => {
 
             console.log("il valore del checkbox è " + rememberPassword)
 
-            if(rememberLogin == "on"){ 
+            if (rememberLogin == "on") {
                 setCookie("user_email", email, 365); //assegna i cookie
                 setCookie("user_password", password, 365); //assegna i cookie
             } else {
                 setCookie("user_email", email, 0.24); //assegna i cookie
-                setCookie("user_password", password, 0.24); 
+                setCookie("user_password", password, 0.24);
             }
         })
         .catch((error) => {
