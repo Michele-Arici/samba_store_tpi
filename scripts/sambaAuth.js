ef14bf96b060a1e1c6795a7132f77554541d2565
Michele-Arici <50071134+Michele-Arici@users.noreply.github.com>
2022-04-21 22:36:56 +0200

    nuovo salvataggio register + show da finire

--- scripts/sambaAuth.js
+++ scripts/sambaAuth.js
@@ -1,151 +1,157 @@
 import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'https://www.gstatic.com/firebasejs/9.6.10/firebase-auth.js';
+import { ref, push } from "https://www.gstatic.com/firebasejs/9.1.0/firebase-database.js"
 
 //import { addCustomer } from '/scripts/sambaDB.js';
 import { auth } from '/scripts/firebase-config.js';
 import { getCookie, setCookie, eraseCookie } from '/scripts/sambaCookies.js';
 
-//REGISTRAZIONE
-const signupForm = document.querySelector('#signup_form');
-
-signupForm.addEventListener('submit', (e) => {
-    e.preventDefault();
-    //get user info
-    const email = signupForm['signup_email'].value;
-    const password = signupForm['signup_password'].value;
-
-    // Da uncommentare quando è stata aggiunta sta roba alla casella di registrazione
-    // const firstName = signupForm['signup_firstName'].value;
-    // const surname = signupForm['signup_surname'].value;
-    // const phone_number = signupForm['signup_phoneNumber'].value;
-    // const city = signupForm['signup_city'].value;
 
 
 
-    const isCorrectFormat = checkSignUpInvalidSyntax(email, password);
+const email = getCookie('user_email');
 
-    if (isCorrectFormat) {
-        createUserWithEmailAndPassword(auth, email, password)
-            .then((userCredential) => {
-                // Signed in 
-                const user = userCredential.user;
-                console.log("registrazione ok");
-                console.log(user);
-
-                //QUERY PER COLLEGARE L'ACCOUNT!!
-                //addCustomer(user.uid, email, password, phone_number, city, firstName, surname);
-                //QUERY PER COLLEGARE L'ACCOUNT!!
-
-                //Se funziona la registrazione nascondo il modale del signup e mostro quello del signin
-                $('#signUpModal').modal('hide');
-                $('#signInModal').modal('show');
-            })
-            .catch((error) => {
-                const errorCode = error.code;
-                console.log(errorCode);
-
-                if (errorCode == 'auth/email-already-in-use') {
-                    document.getElementById('email_invalid_feed').innerHTML = "Email already in use. Use another one";
-                }
-                if (errorCode == 'auth/weak-password') {
-                    document.getElementById('pass_invalid_feed').innerHTML = "The password is too weak. It has to be at least 6 characters long";
-                }
-            });
+if (email == null) {
+    
+    //REGISTRAZIONE
+    const signupForm = document.querySelector('#signup_form');
+    function checkSignUpInvalidSyntax(email, password) {
+        //Resetting the innerHTML errors
+        document.getElementById('email_invalid_feed').innerHTML = "";
+        document.getElementById('pass_invalid_feed').innerHTML = "";
+        document.getElementById('tos_invalid_feed').innerHTML = "";
+        //Resettingb the innerHTML errors
+    
+    
+        //Checking the syntax
+        let isCorrectFormat = true;
+    
+        if (email == '') {
+            document.getElementById('email_invalid_feed').innerHTML = "Insert a valid email address.";
+            isCorrectFormat = false;
+        }
+        if (password == '') {
+            document.getElementById('pass_invalid_feed').innerHTML = "Insert a password";
+            isCorrectFormat = false;
+        }
+        if (!signupForm['tos_agreement'].checked) {
+            document.getElementById('tos_invalid_feed').innerHTML = "You need to accept TOS in order to continue";
+            isCorrectFormat = false;
+        }
+        return isCorrectFormat;
+    }
+    
+    function checkSignInInvalidSyntax(email, password) {
+        //reset previous innerHTML messages
+        document.getElementById('signin_email_invalid_feed').innerHTML = "";
+        document.getElementById('signin_pass_invalid_feed').innerHTML = "";
+        //reset previous innerHTML messages
+    
+        let isCorrectFormat = true;
+    
+        if (email == '') {
+            document.getElementById('signin_email_invalid_feed').innerHTML = "Insert a valid email address.";
+            isCorrectFormat = false;
+        }
+    
+        if (password == '') {
+            document.getElementById('signin_pass_invalid_feed').innerHTML = "Insert a password";
+            isCorrectFormat = false;
+        }
+        return isCorrectFormat;
     }
 
-});
+    signupForm.addEventListener('submit', (e) => {
+        e.preventDefault();
+        //get user info
+        const email = signupForm['signup_email'].value;
+        const password = signupForm['signup_password'].value;
+
+        // Da uncommentare quando è stata aggiunta sta roba alla casella di registrazione
+        // const firstName = signupForm['signup_firstName'].value;
+        // const surname = signupForm['signup_surname'].value;
+        // const phone_number = signupForm['signup_phoneNumber'].value;
+        // const city = signupForm['signup_city'].value;
 
 
 
-function checkSignUpInvalidSyntax(email, password) {
-    //Resetting the innerHTML errors
-    document.getElementById('email_invalid_feed').innerHTML = "";
-    document.getElementById('pass_invalid_feed').innerHTML = "";
-    document.getElementById('tos_invalid_feed').innerHTML = "";
-    //Resettingb the innerHTML errors
+        const isCorrectFormat = checkSignUpInvalidSyntax(email, password);
+
+        if (isCorrectFormat) {
+            createUserWithEmailAndPassword(auth, email, password)
+                .then((userCredential) => {
+                    // Signed in 
+                    const user = userCredential.user;
+                    console.log("registrazione ok");
+
+                    //QUERY PER COLLEGARE L'ACCOUNT!!
+                    //addCustomer(user.uid, email, password, phone_number, city, firstName, surname);
+                    //QUERY PER COLLEGARE L'ACCOUNT!!
+                    
+                    let json = {email: email, cart: {initialize: ""},image: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/OOjs_UI_icon_userAvatar.svg/2048px-OOjs_UI_icon_userAvatar.svg.png"}
+                    let cartRef = ref(firebase.database(), "customers/");
+                    push(cartRef, json);
+                    
+                    //Se funziona la registrazione nascondo il modale del signup e mostro quello del signin
+                    $('#signUpModal').modal('hide');
+                    $('#signInModal').modal('show');
+                })
+                .catch((error) => {
+                    const errorCode = error.code;
+                    console.log(errorCode);
+
+                    if (errorCode == 'auth/email-already-in-use') {
+                        document.getElementById('email_invalid_feed').innerHTML = "Email already in use. Use another one";
+                    }
+                    if (errorCode == 'auth/weak-password') {
+                        document.getElementById('pass_invalid_feed').innerHTML = "The password is too weak. It has to be at least 6 characters long";
+                    }
+                });
+        }
+
+    });
+
+    const signinForm = document.querySelector('#signin_form');
+
+    signinForm.addEventListener('submit', (e) => {
+        e.preventDefault();
+
+        //get user info
+        const email = signinForm['signin_email'].value;
+        const password = signinForm['signin_password'].value;
+        //const rememberLogin = signinForm['remember_login'].value;
+
+        const isCorrectFormat = checkSignInInvalidSyntax(email, password);
 
 
-    //Checking the syntax
-    let isCorrectFormat = true;
+        if (isCorrectFormat) {
+            signInWithEmailAndPassword(auth, email, password)
+                .then((userCredential) => {
+                    // Signed in 
+                    const user = userCredential.user;
+                    console.log(user);
+                    console.log("loggato");
 
-    if (email == '') {
-        document.getElementById('email_invalid_feed').innerHTML = "Insert a valid email address.";
-        isCorrectFormat = false;
-    }
-    if (password == '') {
-        document.getElementById('pass_invalid_feed').innerHTML = "Insert a password";
-        isCorrectFormat = false;
-    }
-    if (!signupForm['tos_agreement'].checked) {
-        document.getElementById('tos_invalid_feed').innerHTML = "You need to accept TOS in order to continue";
-        isCorrectFormat = false;
-    }
-    return isCorrectFormat;
+                    if (document.getElementById('remember_login').checked) {
+                        //(nomeCookie, valore che vuoi dare al cookie, durata cookie: 1 equivale ad 1 giorno)
+                        setCookie("user_email", email, 365);
+                        setCookie("user_password", password, 365);
+                    } else {
+                        setCookie("user_email", email, 0.24);
+                        setCookie("user_password", password, 0.24);
+                    }
+
+                    location.reload(); //Aggiorna la pagina
+                })
+                .catch((error) => {
+                    const errorCode = error.code;
+
+                    if (errorCode == 'auth/wrong-password' || errorCode == 'auth/user-not-found') {
+                        document.getElementById('signin_check_invalid_feed').innerHTML = "Incorrect email or password";
+                    }
+                    console.log("sheesh");
+                });
+        }
+    });
 }
 
 
-const signinForm = document.querySelector('#signin_form');
-
-signinForm.addEventListener('submit', (e) => {
-    e.preventDefault();
-
-    //get user info
-    const email = signinForm['signin_email'].value;
-    const password = signinForm['signin_password'].value;
-    //const rememberLogin = signinForm['remember_login'].value;
-
-    const isCorrectFormat = checkSignInInvalidSyntax(email, password);
-
-
-    if (isCorrectFormat) {
-        signInWithEmailAndPassword(auth, email, password)
-            .then((userCredential) => {
-                // Signed in 
-                const user = userCredential.user;
-                console.log(user);
-                console.log("loggato");
-
-                if (document.getElementById('remember_login').checked) {
-                    //(nomeCookie, valore che vuoi dare al cookie, durata cookie: 1 equivale ad 1 giorno)
-                    setCookie("user_email", email, 365);
-                    setCookie("user_password", password, 365);
-                } else {
-                    setCookie("user_email", email, 0.24);
-                    setCookie("user_password", password, 0.24);
-                }
-
-                location.reload(); //Aggiorna la pagina
-            })
-            .catch((error) => {
-                const errorCode = error.code;
-
-                if (errorCode == 'auth/wrong-password' || errorCode == 'auth/user-not-found') {
-                    document.getElementById('signin_check_invalid_feed').innerHTML = "Incorrect email or password";
-                }
-                console.log("sheesh");
-            });
-
-    }
-
-
-
-});
-
-function checkSignInInvalidSyntax(email, password) {
-    //reset previous innerHTML messages
-    document.getElementById('signin_email_invalid_feed').innerHTML = "";
-    document.getElementById('signin_pass_invalid_feed').innerHTML = "";
-    //reset previous innerHTML messages
-
-    let isCorrectFormat = true;
-
-    if (email == '') {
-        document.getElementById('signin_email_invalid_feed').innerHTML = "Insert a valid email address.";
-        isCorrectFormat = false;
-    }
-
-    if (password == '') {
-        document.getElementById('signin_pass_invalid_feed').innerHTML = "Insert a password";
-        isCorrectFormat = false;
-    }
-    return isCorrectFormat;
-}
---
+++ scripts/sambaSignInButton.js
@@ -0,0 +1,169 @@
+import { getCookie, setCookie, eraseCookie } from './sambaCookies.js';
+
+function logout() {
+    eraseCookie("user_email");
+    eraseCookie("user_password");
+
+    location.reload();
+}
+
+const email = getCookie('user_email');
+
+if (email != null) {
+    //if you're logged in you'll see this
+
+    let div = `<div class="nav-item dropdown">
+                <a href="#" class="nav-link d-flex lh-1 text-reset p-0" data-bs-toggle="dropdown" aria-label="Open user menu">
+                    <span class="avatar avatar-sm" style="background-image: url(https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/OOjs_UI_icon_userAvatar.svg/2048px-OOjs_UI_icon_userAvatar.svg.png)"></span>
+                    <div class="d-none d-xl-block ps-2">
+                        <div><strong>${email}</strong></div>
+                    </div>
+                </a>
+                <div class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
+                    <a class="dropdown-item" id="logout_dropdown">Logout</a>
+                </div>
+            </div>`;
+    document.getElementById('profile_button_browse').innerHTML = div;
+
+    document.getElementById('logout_dropdown').addEventListener('click', (e) => {
+        e.preventDefault();
+
+        logout()
+    });
+
+    let cart = `<li class="nav-item">
+                <a class="nav-link" href="./cart.html">
+                    <span class="nav-link-icon d-md-none d-lg-inline-block">
+                        <svg xmlns="http://www.w3.org/2000/svg"
+                            class="icon icon-tabler icon-tabler-shopping-cart" width="24" height="24"
+                            viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none"
+                            stroke-linecap="round" stroke-linejoin="round">
+                            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                            <circle cx="6" cy="19" r="2"></circle>
+                            <circle cx="17" cy="19" r="2"></circle>
+                            <path d="M17 17h-11v-14h-2"></path>
+                            <path d="M6 5l14 1l-1 7h-13"></path>
+                        </svg>
+                    </span>
+                    <span class="nav-link-title">
+                        Cart
+                    </span>
+                </a>
+            </li>`;
+    document.getElementById('cart_li').innerHTML = cart;    
+} else {
+    let div = `<div class="btn-list">
+                <a href="#" class="btn btn-primary d-none d-sm-inline-block"
+                    data-bs-toggle="modal" data-bs-target="#signInModal">
+                    <svg xmlns="http://www.w3.org/2000/svg"
+                        class="icon icon-tabler icon-tabler-user" width="24" height="24"
+                        viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none"
+                        stroke-linecap="round" stroke-linejoin="round">
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                        <circle cx="12" cy="7" r="4"></circle>
+                        <path d="M6 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2"></path>
+                    </svg> Sign-in
+                </a>
+            </div>`;
+    document.getElementById('profile_button_browse').innerHTML = div;
+
+    let modals = `<!-- SIGNUP MODAL -->
+    <div class="modal fade" id="signUpModal" tabindex="-1" style="display: none;" aria-hidden="true">
+        <div class="modal-dialog modal-lg modal-dialog-centered" role="document">
+            <div class="modal-content">
+                <div class="modal-body d-flex flex-column">
+                    <div class="container-tight py-4">
+                        <form class="card card-md" id="signup_form">
+                            <div class="card-body">
+                                <h2 class="card-title text-center mb-4">Create new account</h2>
+                                <div class="mb-3">
+                                    <label class="form-label">Email address</label>
+                                    <input type="email" id="signup_email" class="form-control" placeholder="Enter email">
+                                    <div style="display: block;" class="invalid-feedback" id="email_invalid_feed"></div>
+                                </div>
+                                <div class="mb-3">
+                                    <label class="form-label">Password</label>
+                                    <div class="input-group input-group-flat">
+                                        <input type="password" id="signup_password" class="form-control" placeholder="Password" autocomplete="off">
+                                    </div>
+                                    <div style="display: block;" class="invalid-feedback" id="pass_invalid_feed"></div>
+                                </div>
+                                <div class="mb-3">
+                                    <label class="form-check">
+                                        <input type="checkbox" class="form-check-input" id="tos_agreement" />
+                                        <span class="form-check-label">Agree the <a href="tos.html" target="_blank"
+                                                tabindex="-1">terms and policy</a>.</span>
+                                    </label>
+                                    <div style="display: block;" class="invalid-feedback" id="tos_invalid_feed"></div>
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn me-auto" data-bs-dismiss="modal">Close</button>
+                                    <button type="submit" class="btn btn-primary" >Create new
+                                        account</button>
+                                </div>
+                            </div>
+                        </form>
+                        <div class="text-center text-muted mt-3">
+                            Already have account? <a href="#" data-bs-toggle="modal" data-bs-target="#signInModal">Sign
+                                in</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+    <!-- SIGNUP MODAL -->
+
+
+    <!-- SIGNIN MODAL -->
+    <div class="modal fade" id="signInModal" tabindex="-1" style="display: none;" aria-hidden="true">
+        <div class="modal-dialog modal-lg modal-dialog-centered" role="document">
+            <div class="modal-content">
+                <div class="modal-body d-flex flex-column">
+                    <div class="container-tight py-4">
+                        <form class="card card-md" id="signin_form">
+                            <div class="card-body">
+                                <h2 class="card-title text-center mb-4">Login to your account</h2>
+                                <div class="mb-3">
+                                    <label class="form-label">Email address</label>
+                                    <input type="email" id="signin_email" class="form-control"
+                                        placeholder="Enter email">
+                                    <div style="display: block;" class="invalid-feedback" id="signin_email_invalid_feed"></div>
+                                </div>
+                                <div class="mb-3">
+                                    <label class="form-label">
+                                        Password
+                                    </label>
+                                    <div class="input-group input-group-flat">
+                                        <input type="password" id="signin_password" class="form-control"
+                                            placeholder="Password">
+                                    </div>
+                                    <div style="display: block;" class="invalid-feedback" id="signin_pass_invalid_feed"></div>
+                                </div>
+                                <div class="mb-3">
+                                    <label class="form-check">
+                                        <input type="checkbox" class="form-check-input" id="remember_login" />
+                                        <span class="form-check-label">Remember me on this device</span>
+                                    </label>
+                                    <div style="display: block;" class="invalid-feedback" id="signin_check_invalid_feed"></div>
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn me-auto" data-bs-dismiss="modal">Close</button>
+                                    <button type="submit" class="btn btn-primary">Sign-in</button>
+                                </div>
+                            </div>
+                        </form>
+                        <div class="text-center text-muted mt-3">
+                            Don't have account yet? 
+                            <a href="#" data-bs-toggle="modal" data-bs-target="#signUpModal">Sign up</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- SIGNIN MODAL -->`;
+    
+    document.getElementById('modals_sign_div').innerHTML = modals;
+}
